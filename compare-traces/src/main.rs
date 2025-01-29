32361ab75eedd303bf242dd3eab8a8efaf991eed
J. Ryan Stinnett <jryans@gmail.com>
2025-01-29 22:07:56 +0000

    Skip equal regions, focus on changes only

--- compare-traces/src/main.rs
+++ compare-traces/src/main.rs
@@ -42,35 +42,39 @@ fn main() -> Result<()> {
         .algorithm(similar::Algorithm::Patience)
         .diff_lines(&before_content,&after_content);
 
-    for op in diff.ops() {
-        let change_tuples: Vec<_> = op
-            .iter_slices(diff.old_slices(), diff.new_slices())
-            .collect();
-        if op.tag() == DiffTag::Replace {
-            assert!(change_tuples.len() == 2);
-            for (tag, slices) in change_tuples {
-                let (sign, style) = match tag {
-                    ChangeTag::Delete => ("<", Style::new().magenta()),
-                    ChangeTag::Insert => (">", Style::new().yellow()),
-                    ChangeTag::Equal => unreachable!(),
-                };
-                for slice in slices {
-                    print!("{}{}", style.apply_to(sign).bold(), style.apply_to(slice));
+    // TODO: Add `context` option to reveal surrounding lines when desired
+    for op_group in diff.grouped_ops(0) {
+        for op in op_group {
+            let change_tuples: Vec<_> = op
+                .iter_slices(diff.old_slices(), diff.new_slices())
+                .collect();
+            if op.tag() == DiffTag::Replace {
+                assert!(change_tuples.len() == 2);
+                for (tag, slices) in change_tuples {
+                    let (sign, style) = match tag {
+                        ChangeTag::Delete => ("<", Style::new().magenta()),
+                        ChangeTag::Insert => (">", Style::new().yellow()),
+                        ChangeTag::Equal => unreachable!(),
+                    };
+                    for slice in slices {
+                        print!("{}{}", style.apply_to(sign).bold(), style.apply_to(slice));
+                    }
                 }
-            }
-        } else {
-            assert!(change_tuples.len() == 1);
-            for (tag, slices) in change_tuples {
-                let (sign, style) = match tag {
-                    ChangeTag::Delete => ("-", Style::new().red()),
-                    ChangeTag::Insert => ("+", Style::new().green()),
-                    ChangeTag::Equal => (" ", Style::new()),
-                };
-                for slice in slices {
-                    print!("{}{}", style.apply_to(sign).bold(), style.apply_to(slice));
+            } else {
+                assert!(change_tuples.len() == 1);
+                for (tag, slices) in change_tuples {
+                    let (sign, style) = match tag {
+                        ChangeTag::Delete => ("-", Style::new().red()),
+                        ChangeTag::Insert => ("+", Style::new().green()),
+                        ChangeTag::Equal => (" ", Style::new()),
+                    };
+                    for slice in slices {
+                        print!("{}{}", style.apply_to(sign).bold(), style.apply_to(slice));
+                    }
                 }
             }
         }
+        println!("---");
     }
 
     Ok(())
