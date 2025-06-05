a5752d08b1a823f3b726c371cef7f4d9ebe96ab5
J. Ryan Stinnett <jryans@gmail.com>
2025-06-05 17:24:24 +0100

    Remove initial (broken) tree diffing attempt

--- compare-traces/src/tree.rs
+++ compare-traces/src/tree.rs
@@ -4,7 +4,7 @@ use std::ops::{Index, IndexMut};
 
 use bimap::BiHashMap;
 use indexmap::IndexMap;
-use similar::{capture_diff_slices, DiffOp, TextDiff};
+use similar::{capture_diff_slices, DiffOp};
 
 use crate::event::{line_depth, Event, Eventable};
 
@@ -580,194 +580,6 @@ pub struct TreeDiff<'content> {
     diff_ops: Vec<TreeDiffOp>,
 }
 
-fn compare_frames(
-    before_frame: Vec<(usize, &str)>,
-    after_frame: Vec<(usize, &str)>,
-) -> Vec<TreeDiffOp> {
-    let mut tree_diff_ops = Vec::new();
-
-    // Create faux documents for text diffing
-    // JRS: There's surely a better way to diff the frames than this...
-    let before_frame_content = before_frame
-        .iter()
-        .map(|(_, s)| *s)
-        .collect::<Vec<_>>()
-        .join("\n");
-    let after_frame_content = after_frame
-        .iter()
-        .map(|(_, s)| *s)
-        .collect::<Vec<_>>()
-        .join("\n");
-
-    let diff = TextDiff::configure()
-        .algorithm(similar::Algorithm::Patience)
-        .diff_lines(&before_frame_content, &after_frame_content);
-
-    // Transform text diffs of frames into to tree diffs,
-    // converting indices from our faux documents into those for the entire tree
-    for text_diff_op_group in diff.grouped_ops(0) {
-        for text_diff_op in text_diff_op_group {
-            match text_diff_op {
-                DiffOp::Insert {
-                    new_index, new_len, ..
-                } => {
-                    for i in 0..new_len {
-                        tree_diff_ops.push(TreeDiffOp::Add {
-                            after_index: after_frame[new_index + i].0,
-                        });
-                    }
-                }
-                DiffOp::Delete {
-                    old_index, old_len, ..
-                } => {
-                    for i in 0..old_len {
-                        tree_diff_ops.push(TreeDiffOp::Remove {
-                            before_index: before_frame[old_index + i].0,
-                        });
-                    }
-                }
-                DiffOp::Replace {
-                    old_index,
-                    old_len,
-                    new_index,
-                    new_len,
-                } => {
-                    assert!(old_len == new_len);
-                    for i in 0..old_len {
-                        tree_diff_ops.push(TreeDiffOp::Replace {
-                            before_index: before_frame[old_index + i].0,
-                            after_index: after_frame[new_index + i].0,
-                        });
-                    }
-                }
-                DiffOp::Equal { .. } => {}
-            }
-        }
-    }
-
-    tree_diff_ops
-}
-
-pub fn diff_tree<'content>(
-    before_content: &'content str,
-    after_content: &'content str,
-) -> TreeDiff<'content> {
-    let before_lines: Vec<_> = before_content.lines().collect();
-    let after_lines: Vec<_> = after_content.lines().collect();
-
-    let mut before_index: usize = 0;
-    let mut after_index: usize = 0;
-
-    // For each side in the comparison,
-    // we track a list of events seen at each stack depth.
-    let mut before_stack: Vec<Vec<(usize, &str)>> = Vec::new();
-    let mut after_stack: Vec<Vec<(usize, &str)>> = Vec::new();
-
-    let mut ops = Vec::new();
-
-    // TODO: Compare lists when a stack frame ends...
-    // ...but likely to end at different times on each side if moving line by line...
-    // TODO: If one side returns and re-calls same function,
-    // don't just let that go like nothing happened...!
-
-    // Gather stack frames from each side of the tree.
-    // When both sides return from a frame, compare to look for differences.
-    // TODO: Test all cases below
-    let before_len = before_lines.len();
-    let after_len = after_lines.len();
-    while before_index < before_len && after_index < after_len {
-        let before_line = before_lines[before_index];
-        let after_line = after_lines[after_index];
-
-        // Depth is 1-based
-        let before_line_depth = line_depth(before_line);
-        let after_line_depth = line_depth(after_line);
-
-        assert!(before_stack.len() == after_stack.len());
-        if before_line_depth == after_line_depth {
-            let line_depth = before_line_depth;
-            let stack_depth = before_stack.len();
-            if line_depth > stack_depth {
-                before_stack.push(Vec::new());
-                after_stack.push(Vec::new());
-            } else if line_depth < stack_depth {
-                let complete_before_frame = before_stack.pop().unwrap();
-                let complete_after_frame = after_stack.pop().unwrap();
-                ops.append(&mut compare_frames(
-                    complete_before_frame,
-                    complete_after_frame,
-                ));
-            }
-            let before_frame = before_stack.last_mut().unwrap();
-            before_frame.push((before_index, before_line));
-            before_index += 1;
-            let after_frame = after_stack.last_mut().unwrap();
-            after_frame.push((after_index, after_line));
-            after_index += 1;
-        } else {
-            // We ignore deeper sub-trees when depth is not matched.
-            // Every descent in our tree is starts with a "call from"
-            // at the current depth, so we make use of this semantic here.
-            // TODO: Test one side having more elements at level below the root
-            if before_line_depth < after_line_depth {
-                after_index += 1;
-            } else {
-                before_index += 1;
-            }
-        }
-        assert!(before_stack.len() == after_stack.len());
-    }
-
-    // For now, let's just assume before is the longer side
-    // TODO: Support longer after trace as well
-    // JRS: Try to merge this into the twinned loop above
-    assert!(after_index == after_len);
-    while before_index < before_len {
-        let before_line = before_lines[before_index];
-
-        let line_depth = line_depth(before_line);
-        let stack_depth = before_stack.len();
-
-        // JRS: Essentially the same code as the twinned loop,
-        // but only thinking about one side
-        assert!(before_stack.len() == after_stack.len());
-        if line_depth > stack_depth {
-            before_stack.push(Vec::new());
-            after_stack.push(Vec::new());
-        } else if line_depth < stack_depth {
-            let complete_before_frame = before_stack.pop().unwrap();
-            let complete_after_frame = after_stack.pop().unwrap();
-            ops.append(&mut compare_frames(
-                complete_before_frame,
-                complete_after_frame,
-            ));
-        }
-        let before_frame = before_stack.last_mut().unwrap();
-        before_frame.push((before_index, before_line));
-        before_index += 1;
-        assert!(before_stack.len() == after_stack.len());
-    }
-
-    // Traces often trail off without returning to starting depth,
-    // so pop and compare any remaining frames
-    assert!(before_stack.len() == after_stack.len());
-    while !before_stack.is_empty() {
-        let complete_before_frame = before_stack.pop().unwrap();
-        let complete_after_frame = after_stack.pop().unwrap();
-        ops.append(&mut compare_frames(
-            complete_before_frame,
-            complete_after_frame,
-        ));
-    }
-
-    TreeDiff {
-        before_lines,
-        after_lines,
-        edit_ops: Default::default(),
-        diff_ops: ops,
-    }
-}
-
 /// Override `Event` equality to allow for some source coordinate drift
 #[derive(Clone)]
 struct FuzzyEvent(Event);
@@ -1245,8 +1057,9 @@ fn find_position_in_parent(
     before_sibling_position + 1
 }
 
+/// Diff tree using the Chawathe et al. approach
 // TODO: Separate diff algorithm from tree events to simplify testing and reuse
-pub fn diff_tree_chawathe<'content>(
+pub fn diff_tree<'content>(
     before_content: &'content str,
     after_content: &'content str,
 ) -> TreeDiff<'content> {
@@ -1841,7 +1654,7 @@ CF: D at file.tex
     CF: Se at file.tex
     CF: Sg at file.tex"
             .trim();
-        let diff = diff_tree_chawathe(before_content, after_content);
+        let diff = diff_tree(before_content, after_content);
         assert_eq!(
             diff.edit_ops,
             vec![
@@ -1883,7 +1696,7 @@ CF: all_attrs_init at attr.c:155:3
   RF: hashmap_iter_next at hashmap.c:308:1"
             .trim();
 
-        let diff = diff_tree_chawathe(before_content, after_content);
+        let diff = diff_tree(before_content, after_content);
         assert_eq!(
             diff.edit_ops,
             vec![
@@ -1940,7 +1753,7 @@ CF: system_path at exec-cmd.c:268:27
   RF: system_prefix at exec-cmd.c:248:2"
             .trim();
 
-        let diff = diff_tree_chawathe(before_content, after_content);
+        let diff = diff_tree(before_content, after_content);
         assert_eq!(
             diff.edit_ops,
             vec![
